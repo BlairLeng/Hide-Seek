3c4a1884247b6c800257d96e65aeffecc8818005
Ziang Leng <zaleng@bu.edu>
2020-02-12 14:11:29 -0500

    Scene

---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze{
+    // this shows how many different numbers we want to have on x axis and y axis
+    public int x = 0;
+    public int y = 0;
+    // this shows how for each [x,y], what do we have on top
+    public int[,] grids = new int[y,x];
+    public Maze(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setGrid(int x, int y, int type){
+        this.grids[y,x] = type;
+    }
+
+
+}
