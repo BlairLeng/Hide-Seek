d1ca9e07ba47bab300e4439b4a3463b6777533b0
xxggxxgan <xxggxxgan@gmail.com>
2020-04-12 19:32:26 -0400

    maze

--- Assets/Scripts/Frontend/GridManger.cs
+++ Assets/Scripts/Frontend/GridManger.cs
@@ -16,29 +16,45 @@ public class GridManger : MonoBehaviour
         Columns = Horizontal * 2;
         Rows = Vertical * 2;
         Grid = new int[Columns, Rows];
-        for(int i = 0; i< Columns; i++)
-        {
-            for (int j = 0; j < Rows; j++)
-            {
-                Grid[i, j] = 1;
-                SpawnTile(i, j, 1);
-            }
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //for (int i = 0; i < Columns; i++)
+        //{
+        //    for (int j = 0; j < Rows; j++)
+        //    {
+        //        Grid[i, j] = 1;
+        //        SpawnTile(i, j, 1);
+        //    }
+        //}
+        //SpawnTile(0, 1, 1);
     }
 
     // get x y and spwan the tile
-    private void SpawnTile(int x,int y,int Value)
+    public void SpawnTile(int x,int y,int Value)
     {
+        Sprite sp = Resources.Load<Sprite>("Sprites/Square");
         GameObject g = new GameObject("X" + x+ "Y"+y);
         g.transform.position = new Vector3(x-(Horizontal - 0.5f),y-(Vertical - 0.5f));
-        var s = g.AddComponent<SpriteRenderer>();
-        s.sprite = sprite;
-        s.color = new Color(1.0f, 1.0f, 1.0f);
+        var s = g.AddComponent<SpriteRenderer>(); 
+        //Debug.Log("sprite should be "+ sp);
+        s.sprite = sp;
+        if (Value == 0)
+        {
+            s.color = new Color(1.0f, 1.0f, 1.0f);
+        }
+        else if (Value == 1)
+        {
+            s.color = new Color(0.0f, 0.0f, 0.0f);
+        }
+
+    }
+
+    public void spawnentire()
+    {
+        return;
     }
 }
--- Assets/Scripts/Frontend/view.cs
+++ Assets/Scripts/Frontend/view.cs
@@ -4,36 +4,42 @@
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
-    using UnityEngine.UI;
+
     
 
-    public class view 
+    public class view
     {
-        //-1 to separate
-    
-        public GameObject ob;
+        
+
+
+        GameObject g = new GameObject();
         public static void receiver(string msg)
         {
             MyClass get_grid = JsonUtility.FromJson<MyClass>(msg);
 
             Debug.Log(msg);
-            Debug.Log(get_grid.grids[0].x);
-            Debug.Log(get_grid.grids[1].x);
+            //Debug.Log("x is "+get_grid.grids[0].x);
+            //Debug.Log("y is "+get_grid.grids[0].y);
+            //Debug.Log("type is "+ get_grid.grids[0].type);
+
+            GameObject gmob = new GameObject();
+            GridManger gm = gmob.AddComponent<GridManger>();
+            gm.SpawnTile(get_grid.grids[0].x, get_grid.grids[0].y, get_grid.grids[0].type);
+            int len = get_grid.width * get_grid.height;
+            Random random = new Random();
+            int rd = Random.Range(0,2);
+            for (int i = 0; i < len; i++)
+            {
+                gm.SpawnTile(get_grid.grids[i].x, get_grid.grids[i].y, Random.Range(0, 2));
+            }
 
 
 
+                //Debug.Log(msg[0]);
+                return;
 
-            //Debug.Log(msg[0]);
-            return;
-
-            //split
-            //info1
-            //call xxx function
-
-            //info2 
-            //call xxx function
-
-            //....
+            
+        
 
         }
 
